0a67e07b40a51da82659e000305ff1d8f9774a0e
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-09-04 15:21:25 +0300

    Add user registration endpoint and user model with password hashing

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
+import bcrypt from 'bcryptjs'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import User from './models/User.js'
 
 dotenv.config()
 const app = express()
@@ -22,5 +24,31 @@ app.get('/api/test', (req, res) => {
   res.json({ message: 'Hello from Backend' })
 })
 
+app.post('/api/auth/register', async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+    if (existingUser)
+      return res.status(400).json({ error: 'Email already in use' })
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword
+    })
+
+    res.status(201).json({
+      id: newUser._id,
+      username: newUser.username,
+      email: newUser.email
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ message: 'Server error' })
+  }
+})
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server runnning on port ${PORT}`))
