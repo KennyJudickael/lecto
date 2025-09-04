5025d8d87e7e220bb4a8cf0ae2984fea41e1ea83
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-09-04 11:31:37 +0300

    Initial setup: Next.js frontend + Express backend + MongoDB connected

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+
+dotenv.config()
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('MongoDB connecteed'))
+  .catch((err) => console.error('DB connection error', err))
+
+app.get('/', (req, res) => {
+  res.send('Backend runnning')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server runnning on port ${PORT}`))
