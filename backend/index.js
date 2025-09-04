396042ef6cc0b82dacded8fe1aa1380328450ae8
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-09-04 14:55:56 +0300

    Implement API endpoint and update frontend to fetch data

--- backend/index.js
+++ backend/index.js
@@ -18,5 +18,9 @@ app.get('/', (req, res) => {
   res.send('Backend runnning')
 })
 
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from Backend' })
+})
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server runnning on port ${PORT}`))
